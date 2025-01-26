e9193907f8ba5e7bae01db88dd3416f0e4a088cb
jonitreak <66328347+jonitreak@users.noreply.github.com>
2025-01-27 00:17:56 +0100

    Ajout d'animation pour le personnage. Ajout de collision pour certains objet Ajout de mouvement dans 8 directions

--- Scenes/Player/Scripts/player.gd
+++ Scenes/Player/Scripts/player.gd
@@ -1,29 +1,53 @@
 extends CharacterBody2D
 
 
-const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
-
+const max_speed = 200
+var last_direction:=Vector2(1,0)
 
 func _physics_process(delta: float) -> void:
-	# Add the gravity.
-	#if not is_on_floor():
-		#velocity += get_gravity() * delta
-
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var x_direction := Input.get_axis("ui_left", "ui_right")
-	if x_direction:
-		velocity.x = x_direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		
-		
-	var y_direction := Input.get_axis("ui_up", "ui_down")
-	if y_direction:
-		velocity.y = y_direction * SPEED
-	else:
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-
+	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
+	velocity= direction*max_speed
 	move_and_slide()
+	
+	if direction.length()>0:
+		last_direction=direction
+		play_walk_animation(direction)
+	else : 
+		play_idle_animation(last_direction)
+	
+func play_walk_animation(direction): 
+	if direction.x>0 and direction.y==0 : 
+		$AnimatedSprite2D.play("WalkAnimationRight")
+	elif direction.x>0 and direction.y>0 : 
+		$AnimatedSprite2D.play("WalkAnimationDownRight")
+	elif direction.x>0 and direction.y<0 : 
+		$AnimatedSprite2D.play("WalkAnimationUpRight")
+	elif direction.x<0 and direction.y==0: 
+		$AnimatedSprite2D.play("WalkAnimationLeft")
+	elif direction.x<0 and direction.y>0 : 
+		$AnimatedSprite2D.play("WalkAnimationDownLeft")
+	elif direction.x<0 and direction.y<0 : 
+		$AnimatedSprite2D.play("WalkAnimationUpLeft")
+	if direction.x==0 and direction.y>0 : 
+		$AnimatedSprite2D.play("WalkAnimationDown")
+	if direction.x==0 and direction.y<0 : 
+		$AnimatedSprite2D.play("WalkAnimationUp")
+		
+func play_idle_animation(direction): 
+	if direction.x>0 and direction.y==0 : 
+		$AnimatedSprite2D.play("IdleAnimationRight")
+	elif direction.x>0 and direction.y>0 : 
+		$AnimatedSprite2D.play("IdleAnimationDownRight")
+	elif direction.x>0 and direction.y<0 : 
+		$AnimatedSprite2D.play("IdleAnimationUpRight")
+	elif direction.x<0 and direction.y==0: 
+		$AnimatedSprite2D.play("IdleAnimationLeft")
+	elif direction.x<0 and direction.y>0 : 
+		$AnimatedSprite2D.play("IdleAnimationDownLeft")
+	elif direction.x<0 and direction.y<0 : 
+		$AnimatedSprite2D.play("IdleAnimationUpLeft")
+	if direction.x==0 and direction.y>0 : 
+		$AnimatedSprite2D.play("IdleAnimationDown")
+	if direction.x==0 and direction.y<0 : 
+		$AnimatedSprite2D.play("IdleAnimationUp")
+		
