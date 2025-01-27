4f28b9a6f6ffc4b9c58c4cff62baef9b7fa15115
jonitreak <66328347+jonitreak@users.noreply.github.com>
2025-01-27 15:38:51 +0100

    Ajout des SwordWalk et SwordIdle animations ainsi qu'une metadonnée permettant de savoir quelle animation utiliser. L'épée est pour l'instant visible seulement sur quelque idle animations

--- Scenes/Player/Scripts/player.gd
+++ Scenes/Player/Scripts/player.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 const max_speed = 200
 var last_direction:=Vector2(1,0)
+var sword:=bool(false)
 
 func _physics_process(delta: float) -> void:
 	var direction = Input.get_vector("ui_left","ui_right","ui_up","ui_down")
@@ -14,40 +15,88 @@ func _physics_process(delta: float) -> void:
 		play_walk_animation(direction)
 	else : 
 		play_idle_animation(last_direction)
-	
-func play_walk_animation(direction): 
-	if direction.x>0 and direction.y==0 : 
-		$AnimatedSprite2D.play("WalkAnimationRight")
-	elif direction.x>0 and direction.y>0 : 
-		$AnimatedSprite2D.play("WalkAnimationDownRight")
-	elif direction.x>0 and direction.y<0 : 
-		$AnimatedSprite2D.play("WalkAnimationUpRight")
-	elif direction.x<0 and direction.y==0: 
-		$AnimatedSprite2D.play("WalkAnimationLeft")
-	elif direction.x<0 and direction.y>0 : 
-		$AnimatedSprite2D.play("WalkAnimationDownLeft")
-	elif direction.x<0 and direction.y<0 : 
-		$AnimatedSprite2D.play("WalkAnimationUpLeft")
-	if direction.x==0 and direction.y>0 : 
-		$AnimatedSprite2D.play("WalkAnimationDown")
-	if direction.x==0 and direction.y<0 : 
-		$AnimatedSprite2D.play("WalkAnimationUp")
+
+func play_walk_animation(direction):
+	if self.get_meta("sword") : 
+		play_sword_walk_animation(direction)
+	else : 
+		play_basic_walk_animation(direction)
 		
-func play_idle_animation(direction): 
+func play_idle_animation(direction):
+	if self.get_meta("sword") : 
+		play_sword_idle_animation(direction)
+	else : 
+		play_basic_idle_animation(direction)
+		
+func play_basic_walk_animation(direction): 
 	if direction.x>0 and direction.y==0 : 
-		$AnimatedSprite2D.play("IdleAnimationRight")
+		$AnimatedSprite2D.play("BasicWalkAnimationRight")
 	elif direction.x>0 and direction.y>0 : 
-		$AnimatedSprite2D.play("IdleAnimationDownRight")
+		$AnimatedSprite2D.play("BasicWalkAnimationDownRight")
 	elif direction.x>0 and direction.y<0 : 
-		$AnimatedSprite2D.play("IdleAnimationUpRight")
+		$AnimatedSprite2D.play("BasicWalkAnimationUpRight")
 	elif direction.x<0 and direction.y==0: 
-		$AnimatedSprite2D.play("IdleAnimationLeft")
+		$AnimatedSprite2D.play("BasicWalkAnimationLeft")
 	elif direction.x<0 and direction.y>0 : 
-		$AnimatedSprite2D.play("IdleAnimationDownLeft")
+		$AnimatedSprite2D.play("BasicWalkAnimationDownLeft")
 	elif direction.x<0 and direction.y<0 : 
-		$AnimatedSprite2D.play("IdleAnimationUpLeft")
+		$AnimatedSprite2D.play("BasicWalkAnimationUpLeft")
 	if direction.x==0 and direction.y>0 : 
-		$AnimatedSprite2D.play("IdleAnimationDown")
+		$AnimatedSprite2D.play("BasicWalkAnimationDown")
 	if direction.x==0 and direction.y<0 : 
-		$AnimatedSprite2D.play("IdleAnimationUp")
+		$AnimatedSprite2D.play("BasicWalkAnimationUp")
+		
+func play_basic_idle_animation(direction): 
+	if direction.x>0 and direction.y==0 : 
+		$AnimatedSprite2D.play("BasicIdleAnimationRight")
+	elif direction.x>0 and direction.y>0 : 
+		$AnimatedSprite2D.play("BasicIdleAnimationDownRight")
+	elif direction.x>0 and direction.y<0 : 
+		$AnimatedSprite2D.play("BasicIdleAnimationUpRight")
+	elif direction.x<0 and direction.y==0: 
+		$AnimatedSprite2D.play("BasicIdleAnimationLeft")
+	elif direction.x<0 and direction.y>0 : 
+		$AnimatedSprite2D.play("BasicIdleAnimationDownLeft")
+	elif direction.x<0 and direction.y<0 : 
+		$AnimatedSprite2D.play("BasicIdleAnimationUpLeft")
+	if direction.x==0 and direction.y>0 : 
+		$AnimatedSprite2D.play("BasicIdleAnimationDown")
+	if direction.x==0 and direction.y<0 : 
+		$AnimatedSprite2D.play("BasicIdleAnimationUp")
+		
+func play_sword_walk_animation(direction): 
+	if direction.x>0 and direction.y==0 : 
+		$AnimatedSprite2D.play("SwordWalkAnimationRight")
+	elif direction.x>0 and direction.y>0 : 
+		$AnimatedSprite2D.play("SwordWalkAnimationDownRight")
+	elif direction.x>0 and direction.y<0 : 
+		$AnimatedSprite2D.play("SwordWalkAnimationUpRight")
+	elif direction.x<0 and direction.y==0: 
+		$AnimatedSprite2D.play("SwordWalkAnimationLeft")
+	elif direction.x<0 and direction.y>0 : 
+		$AnimatedSprite2D.play("SwordWalkAnimationDownLeft")
+	elif direction.x<0 and direction.y<0 : 
+		$AnimatedSprite2D.play("SwordWalkAnimationUpLeft")
+	if direction.x==0 and direction.y>0 : 
+		$AnimatedSprite2D.play("SwordWalkAnimationDown")
+	if direction.x==0 and direction.y<0 : 
+		$AnimatedSprite2D.play("SwordWalkAnimationUp")
+		
+func play_sword_idle_animation(direction): 
+	if direction.x>0 and direction.y==0 : 
+		$AnimatedSprite2D.play("SwordIdleAnimationRight")
+	elif direction.x>0 and direction.y>0 : 
+		$AnimatedSprite2D.play("SwordIdleAnimationDownRight")
+	elif direction.x>0 and direction.y<0 : 
+		$AnimatedSprite2D.play("SwordIdleAnimationUpRight")
+	elif direction.x<0 and direction.y==0: 
+		$AnimatedSprite2D.play("SwordIdleAnimationLeft")
+	elif direction.x<0 and direction.y>0 : 
+		$AnimatedSprite2D.play("SwordIdleAnimationDownLeft")
+	elif direction.x<0 and direction.y<0 : 
+		$AnimatedSprite2D.play("SwordIdleAnimationUpLeft")
+	if direction.x==0 and direction.y>0 : 
+		$AnimatedSprite2D.play("SwordIdleAnimationDown")
+	if direction.x==0 and direction.y<0 : 
+		$AnimatedSprite2D.play("SwordIdleAnimationUp")
 		
