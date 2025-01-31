242b16d5862230c024118a0560326ace1dbb45b8
Jaurel Fosset <Jaurel_Fosset@etu.u-bourgogne.fr>
2025-01-31 10:21:17 +0100

    make life bar responsive

--- Scenes/Life bar/life_bar.gd
+++ Scenes/Life bar/life_bar.gd
@@ -13,11 +13,11 @@ var hearts = [
 
 func _ready() -> void:
 	playerState.health_change.connect(_on_health_change)
-	get_node("Control/HeartSprite").texture = hearts[playerState.health]
+	get_node("HeartSprite").texture = hearts[playerState.health]
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_released("take_damage"):
 		playerState.take_damage(1)
 
 func _on_health_change(old_value, new_value):
-	get_node("Control/HeartSprite").texture = hearts[new_value]
+	get_node("HeartSprite").texture = hearts[new_value]
